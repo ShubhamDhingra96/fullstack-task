96290f33ea5f33a8da8f4b05b4cb1a642d6b5e0f
Shubham Dhingra <shubham_dhingra96@yahoo.com>
2026-05-19 01:19:06 -0400

    Full stack task completed

---
+++ backend/items/views.py
@@ -0,0 +1,13 @@
+from rest_framework import generics
+from .models import Item
+from .serializers import ItemSerializer
+
+
+class ItemListCreateView(generics.ListCreateAPIView):
+    queryset = Item.objects.all().order_by("-created_at")
+    serializer_class = ItemSerializer
+
+
+class ItemDetailView(generics.RetrieveUpdateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
